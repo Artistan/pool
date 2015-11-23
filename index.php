395dd54f5fde8d493ffece44f75f7276fee755b4
Charles Peterson <cpeterson@brokerbin.com>
2015-11-23 12:12:08 -0600

    more links

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+include('./pool.cfg.php');
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>D&amp;R Pool Leagues</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-k2/8zcNbxVIh5mnQ52A0r3a6jAgMGxFJFE2707UxGCk= sha512-ZV9KawG2Legkwp3nAlxLIVFudTauWuBpC10uEafMHYL0Sarrz5A7G79kXh5+5+woxQ5HM559XX2UZjMJ36Wplg==" crossorigin="anonymous">
+</head>
+<body>
+<?php $title2='HOME'; include('nav.php'); ?>
+<div class="list-group">
+
+    <a href="leagues.php?id=12" class="list-group-item list-group-item-success">
+        <h4 class="list-group-item-heading">D&amp;R Rochester Pool Leagues</h4>
+
+        <p class="list-group-item-text">
+            Access to standings and schedule forD&amp;R  Rochester Pool Leagues - via D&amp;R
+        </p>
+    </a>
+    <a href="cities.php" class="list-group-item list-group-item-success">
+        <h4 class="list-group-item-heading">All D&amp;R Pool Leagues</h4>
+
+        <p class="list-group-item-text">
+            Access to standings and schedule for ALL D&amp;R Pool Leagues - via D&amp;R
+        </p>
+    </a>
+    <a href="subs.php" class="list-group-item list-group-item-success">
+        <h4 class="list-group-item-heading">SUBS for D&amp;R Pool League</h4>
+
+        <p class="list-group-item-text">
+            Contact D&amp;R/STAR POOL for subs information 800-788-9692 or 507-226-0366 EXT 135
+        </p>
+    </a>
+    <a href="https://calendar.google.com/calendar/embed?wkst=1&src=rrndr8bjo97ohtvpbtsn080fk4@group.calendar.google.com" class="list-group-item list-group-item-warning">
+        <h4 class="list-group-item-heading">D&amp;R Pool Calendar</h4>
+
+        <p class="list-group-item-text">
+            Click the PLUS (+) on this page to add the calendar to you google account.
+        </p>
+    </a>
+    <a href="https://www.momapoolanddarts.com/" class="list-group-item list-group-item-info">
+        <h4 class="list-group-item-heading">MOMA Pool &amp; Darts</h4>
+
+        <p class="list-group-item-text">
+            Access to standings and schedule MOMA
+        </p>
+    </a>
+    <a href="https://goo.gl/BxP6vn" class="list-group-item list-group-item-info">
+        <h4 class="list-group-item-heading">MOMA Pool Poster</h4>
+
+        <p class="list-group-item-text">
+            Ad poster here...
+        </p>
+    </a>
+    <a href="http://dnrstar.com/leagues/pool-league.php" class="list-group-item list-group-item-danger">
+        <h4 class="list-group-item-heading">D&amp;R Pool League Page</h4>
+
+        <p class="list-group-item-text">
+            Competitive Pool Leagues | Southeast Minnesota
+        </p>
+    </a>
+
+</div>
+</body>
+</html>
